348b0f16785e224f60d66dada3119261e46eb03a
markuszcl99 <e_markuszhang@163.com>
2023-12-03 16:43:39 +0800

    mock对象的几种方式 - 补充一下注解的

--- junit-java/src/test/java/com/markus/test/mockito/MockByAnnotationTest.java
+++ junit-java/src/test/java/com/markus/test/mockito/MockByAnnotationTest.java
@@ -1,5 +1,12 @@
 package com.markus.test.mockito;
 
+import com.markus.domain.dto.AccountDto;
+import com.markus.service.AccountLoginService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 /**
  * @author: markus
  * @date: 2023/12/3 4:28 PM
@@ -8,4 +15,18 @@ package com.markus.test.mockito;
  * It's my honor to share what I've learned with you!
  */
 public class MockByAnnotationTest {
+
+    @Mock
+    private AccountLoginService accountLoginService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testAnnotation() {
+        AccountDto account = accountLoginService.findAccount("x", "x");
+        System.out.println(account);
+    }
 }
