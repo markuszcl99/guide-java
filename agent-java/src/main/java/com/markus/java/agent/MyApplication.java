dd72795115f058f31b71ac8717792e0efd580d8d
markuszcl99 <e_markuszhang@163.com>
2024-02-20 13:26:38 +0800

    补充下 java agent 相关的指令

--- agent-java/src/main/java/com/markus/java/agent/MyApplication.java
+++ agent-java/src/main/java/com/markus/java/agent/MyApplication.java
@@ -7,8 +7,9 @@ package com.markus.java.agent;
  */
 public class MyApplication {
   /**
-   * 执行  java -javaagent:target/agent-java-1.0-SNAPSHOT.jar -jar target/agent-java-1.0-SNAPSHOT.jar
-   *
+   * 执行  java -javaagent:target/agent-java-1.0-SNAPSHOT.jar -jar target/agent-java-1.0-SNAPSHOT.jar 会执行代理
+   * 执行 jar tf target/agent-java-1.0-SNAPSHOT.jar 查看 jar 包中的文件
+   * 执行 unzip -p target/agent-java-1.0-SNAPSHOT.jar META-INF/MANIFEST.MF 查看指定文件的内容
    * @param args
    */
   public static void main(String[] args) {
