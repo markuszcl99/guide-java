def1dc8175215384ae7643c438a426f6509826c3
markuszcl99 <e_markuszhang@163.com>
2024-03-14 12:59:14 +0800

    组合模式示例代码更新

---
+++ design-mode-java/src/main/java/com/markus/desgin/mode/structure/Composite/CompositePatternDemo.java
@@ -0,0 +1,30 @@
+package com.markus.desgin.mode.structure.Composite;
+
+/**
+ * @Author: zhangchenglong06
+ * @Date: 2024/3/14
+ * @Description:
+ */
+public class CompositePatternDemo {
+  public static void main(String[] args) {
+    Employee employer = new Employee("employer",25,"老板");
+    Employee employee = new Employee("markus",25,"员工");
+    Employee employee1 = new Employee("markus1",25,"员工");
+    Employee employee2 = new Employee("markus2",25,"员工");
+    Employee employee3 = new Employee("markus3",25,"员工");
+
+    employer.add(employee);
+    employer.add(employee1);
+    employer.add(employee2);
+    employer.add(employee3);
+
+    for (Employee employerEmployee : employer.getEmployees()) {
+      System.out.println(employerEmployee);
+    }
+
+    employer.remove(employee1);
+    for (Employee employerEmployee : employer.getEmployees()) {
+      System.out.println(employerEmployee);
+    }
+  }
+}
---
+++ design-mode-java/src/main/java/com/markus/desgin/mode/structure/Composite/Employee.java
@@ -0,0 +1,44 @@
+package com.markus.desgin.mode.structure.Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: zhangchenglong06
+ * @Date: 2024/3/14
+ * @Description:
+ */
+public class Employee {
+  private String name;
+  private Integer age;
+  private String positionStatement;
+  private List<Employee> employees;
+
+  public Employee(String name, Integer age, String positionStatement) {
+    this.name = name;
+    this.age = age;
+    this.positionStatement = positionStatement;
+    this.employees = new ArrayList<>();
+  }
+
+  public void add(Employee employee) {
+    this.employees.add(employee);
+  }
+
+  public void remove(Employee employee) {
+    this.employees.remove(employee);
+  }
+
+  public List<Employee> getEmployees() {
+    return this.employees;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "name='" + name + '\'' +
+        ", age=" + age +
+        ", positionStatement='" + positionStatement + '\'' +
+        '}';
+  }
+}
