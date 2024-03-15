75ead19c2d7ed1dd8b26774d92b5abea874b35fa
markuszcl99 <e_markuszhang@163.com>
2024-03-15 13:08:49 +0800

    门面模式在 spring 中的应用

--- design-mode-java/src/main/java/com/markus/desgin/mode/structure/facade/FacadePatternDemo.java
+++ design-mode-java/src/main/java/com/markus/desgin/mode/structure/facade/FacadePatternDemo.java
@@ -6,6 +6,12 @@ package com.markus.desgin.mode.structure.facade;
  * @Description:
  */
 public class FacadePatternDemo {
+  /**
+   * 门面模式在 Spring 中的应用:
+   *  org.springframework.web.servlet.DispatcherServlet
+   *  org.springframework.jdbc.core.JdbcTemplate
+   * @param args
+   */
   public static void main(String[] args) {
     RestaurantFacade facade = new RestaurantFacade();
     facade.eat("糖醋排骨");
