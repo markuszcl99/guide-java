502ff9fcdb031407219b5d90cd5fbada948975ff
markuszcl99 <e_markuszhang@163.com>
2024-03-21 12:50:08 +0800

    提交一下模板方法模式的注释

--- design-mode-java/src/main/java/com/markus/desgin/mode/behaviour/TemplateMethodPattern.java
+++ design-mode-java/src/main/java/com/markus/desgin/mode/behaviour/TemplateMethodPattern.java
@@ -6,6 +6,16 @@ package com.markus.desgin.mode.behaviour;
  * @Description:
  */
 public class TemplateMethodPattern {
+  /**
+   * 使用场景：当多个类有共同使用的方法时可以考虑；父类定义行为，子类定义实现
+   * 优点：
+   *  1. 封装不变部分，扩展可变部分
+   *  2. 实现共同代码统一维护
+   *  3. 由父类控制行为，子类实现
+   * 缺点：
+   *  1. 功能越多，子类越多
+   * @param args
+   */
   public static void main(String[] args) {
     Game game = new SkylineGame();
     game.play();
