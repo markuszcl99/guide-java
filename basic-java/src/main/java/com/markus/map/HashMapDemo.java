64db4588599355e8c94bd0c59bebfc31e3cc10c9
markuszcl99 <e_markuszhang@163.com>
2024-04-10 18:30:12 +0800

    map equals 方法

---
+++ basic-java/src/main/java/com/markus/map/HashMapDemo.java
@@ -0,0 +1,21 @@
+package com.markus.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: zhangchenglong06
+ * @Date: 2024/4/10
+ * @Description:
+ */
+public class HashMapDemo {
+  public static void main(String[] args) {
+    Map<String,String> map1 = new HashMap<>();
+    map1.put("1","1");
+
+    Map<String,String> map2 = new HashMap<>();
+    map2.put("1","1");
+
+    System.out.println(map1.equals(map2));
+  }
+}
